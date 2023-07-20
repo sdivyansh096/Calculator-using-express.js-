e32b2bb0eb9c07265c6f5cfc5377fa826172766e
Divyansh Sharma <138120857+sdivyansh096@users.noreply.github.com>
2023-07-20 15:20:16 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app =express();
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static(__dirname+"public"));
+app.use(express.static(__dirname+"/public"));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
